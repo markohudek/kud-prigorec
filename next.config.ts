0080804b9f075dfda614f22f6af2f943c6538451
Marko Huđek <m.hudjek@teamaxess.com>
2024-12-18 21:35:10 +0100

    Add initial styles and configuration files for project setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+  },
 };
 
 export default nextConfig;
